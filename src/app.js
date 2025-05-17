f805f1c54cbcd572714b80ddaff6367feee9acc1
Ravi <ravi.raman@unicorntechmedia.com>
2025-05-17 20:46:47 +0530

    16may

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const app = express();
 
 // till here we have created a server and our server is listening at port 3000.
 
-app.use("/car",(req,res)=>{
+app.use("/car",(req,res)=>{   // request  handler
     res.send("this is mercedes car page siayram");
 });
 
